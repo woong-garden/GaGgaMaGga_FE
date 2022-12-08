534055a237e974ef6821517a2a99446ecaffafa6
GeumBin <lgb9811@gmail.com>
2022-12-08 14:20:53 +0900

    :lipstick:Feat: 별점 선택 추가

--- static/js/review_create.js
+++ static/js/review_create.js
@@ -38,37 +38,3 @@ window.onload = () => {
         readMultipleImage(e.target)
     })
 }
-
-// window.onload = () => {
-//     const realUpload = document.querySelector('.real-upload');
-//     const upload = document.querySelector('.upload');
-
-//     upload.addEventListener('click', () => realUpload.click());
-
-//     function readMultipleImage(input) {
-//         const slideList = document.querySelector('.slidelist')
-//         if (input.files) {
-            
-
-//             const fileArr = Array.from(input.files)
-//             fileArr.forEach(file => {
-//                 const reader = new FileReader()
-
-//                 const liTag = document.createElement('li')
-//                 const image = document.createElement('img')
-//                 image.classList.add('first-img')
-//                 liTag.appendChild(image)
-
-//                 ReadableStream.onload = e => {
-//                     image.src = e.target.result
-//                 }
-//                 reader.readAsDataURL(file)
-//             })
-//             slideList.appendChild(liTag)
-//         }
-//     }
-// //     const multipleImage = document.querySelector('.real-upload')
-// //     multipleImage.addEventListener('change', e=> {
-// //         readMultipleImage(e.target)
-// //     })
-// }
