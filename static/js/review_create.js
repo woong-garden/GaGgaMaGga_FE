98f56bbc9225891432c79893d5a8f3f2b468d96f
GeumBin <lgb9811@gmail.com>
2022-12-14 18:41:16 +0900

    :bug:Fix: 후기 상세 페이지 알람 에러랑 답글 수정 에러 해결
    답글 수정할 때 원래 코멘트 내용에 시간이랑 신고 글자까지 따라붙는 문제 해결
    리뷰 디테일 페이지에서도 실시간 알람 설정 다시 복원
    일단 channels가 윈도우를 지원하지 않으므로 배포 전까지 각주 처리 해둠

--- static/js/review_create.js
+++ static/js/review_create.js
@@ -140,28 +140,3 @@ async function createReview() {
         contentAlert.style.display = "block"
     }
 }
-
-    var formData = new FormData()
-    formData.append("rating_cnt", cls)
-    formData.append("content", content.value)
-    formData.append("review_image_one", images.files[0])
-    formData.append("review_image_two", images.files[1])
-    formData.append("review_image_three", images.files[2])
-
-    const response = await fetch(`${backendBaseUrl}/reviews/${place_id}/`, {
-        headers: {
-            "authorization": "Bearer " + localStorage.getItem("access")
-        },
-        method: 'POST',
-        cache:'no-cache',
-        body: formData
-    })
-
-
-    if (response.status === 201) {
-    alert("후기 등록 완료")
-    window.history.back()}
-    console.log(response)
-    console.log(response.status)
-}
-
